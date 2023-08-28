8858d58fcd717cfd3e937d420331f6d763fe6f98
polyakov-artem <Polyakov.Artem.V@yandex.ru>
2023-08-28 20:29:41 +0300

    fix: fix modal window

--- library/js/modal.js
+++ library/js/modal.js
@@ -1,19 +1,31 @@
 export class Modal {
   constructor(props = {}) {
-    this.classModal = props.classModal || 'modal-block'
-    this.classModalContent = props.classModalContent || 'modal-block__content'
     this.attributeToggler = props.attributeToggler || "data-modal-id";
     this.classModalActive = props.classModalActive || "modal-block_active";
-    this.classCloseBtn = props.classCloseBtn ||  "modal-block__close-btn";
-    this.classViewportLocked = props.classViewportLocked || "locked";
-    this.closeOnOutsideClick = props.closeOnOutsideClick || true;
+    this.classModalWindow = props.classModalWindow || "modal-block__window";
+
+    this.classCloseBtn = props.classCloseBtn || "modal-block__close-btn";
+    this.classOverlay = props.classOverlay || "modal-overlay";
+    this.classOverlayActive =
+      props.classOverlayActive || "modal-overlay_active";
     this.closeClasses = props.closeClasses || [];
 
     this.closeClasses.push(this.classCloseBtn);
 
     this.activeModal = null;
+    this.overlay = null;
+
     this.bodyElement = document.querySelector("body");
+
+    this._createOverlay();
     this.bindEvents();
+  }
+
+  _createOverlay() {
+    if (this.overlay) return;
+    this.overlay = document.createElement("div");
+    this.overlay.classList.add(this.classOverlay);
+    this.bodyElement.append(this.overlay);
   }
 
   bindEvents() {
@@ -23,76 +35,103 @@ export class Modal {
   _clickHandler(e) {
     const element = e.target;
     const toggler = element.closest(`[${this.attributeToggler}]`);
-    const modalContent = element.closest(`.${this.classModalContent}`);
-    const isNotModalContent= !modalContent;
+    const modalWindow = element.closest(`.${this.classModalWindow}`);
+    const isNotModalWindow = !modalWindow;
 
     if (toggler) {
       e.preventDefault();
 
-      const nextModal = document.getElementById(toggler.getAttribute(this.attributeToggler));
+      const nextModal = document.getElementById(
+        toggler.getAttribute(this.attributeToggler)
+      );
       if (!nextModal) return;
 
-      this.activeModal = nextModal;
-      this._open();
-      return;
-    };
-    
+      if (!this.activeModal) {
+        this.activeModal = nextModal;
+        this._open();
+        return;
+      }
+
+      if (this.activeModal && nextModal !== this.activeModal) {
+        this._closeCurrentWindow();
+        this.activeModal = nextModal;
+        this._openNextWindow();
+        return;
+      }
+    }
+
     if (
-        this.activeModal &&
-        this.closeOnOutsideClick && isNotModalContent ||
-        this._hasParentWithClass(element, this.closeClasses)
-      ) {
+      this.activeModal &&
+      (isNotModalWindow || this._hasParentWithClass(element, this.closeClasses))
+    ) {
       this._close();
-      return
+      return;
     }
   }
 
   _open() {
     this._addLock();
-    this._addActiveClasses()
+    this._addModalClasses();
+    this._addOverlayClasses();
   }
 
   _close() {
-    this._removeActiveClasses();
+    this._removeModalClasses();
+    this._removeOverlayClasses();
     this._removeLockAfterAnimation();
     this.activeModal = null;
   }
 
-  _removeLockAfterAnimation(){
-    const modal = this.activeModal;
-    const thisInstance = this;
-    
-    const listener = ()=>{
-      thisInstance._removeLock ();
-      modal.removeEventListener("transitionend", listener)
-    };
-
-    modal.addEventListener("transitionend", listener);
+  _closeCurrentWindow() {
+    this._removeModalClasses();
   }
 
-  _addActiveClasses(){
+  _openNextWindow() {
+    this._addModalClasses();
+  }
+
+  _removeLockAfterAnimation() {
+    const overlay = this.overlay;
+    const thisInstance = this;
+
+    const listener = () => {
+      thisInstance._removeLock();
+      overlay.removeEventListener("transitionend", listener);
+    };
+
+    overlay.addEventListener("transitionend", listener);
+  }
+
+  _addModalClasses() {
     this.activeModal.classList.add(this.classModalActive);
   }
 
-  _removeActiveClasses(){
+  _addOverlayClasses() {
+    this.overlay.classList.add(this.classOverlayActive);
+  }
+
+  _removeModalClasses() {
     this.activeModal.classList.remove(this.classModalActive);
   }
 
-  _hasParentWithClass (element, classes) {
+  _removeOverlayClasses() {
+    this.overlay.classList.remove(this.classOverlayActive);
+  }
+
+  _hasParentWithClass(element, classes) {
     if (!classes.length) return false;
 
     let parent = element;
 
     while (parent !== document) {
-      console.log(parent);
-      if (classes.some(className => parent.classList.contains(className))) {
+      if (classes.some((className) => parent.classList.contains(className))) {
         return true;
-      };
-      
-      parent = parent.parentNode;
-    };
+      }
 
-    return false; 
+      parent = parent.parentNode;
+    }
+
+    return false;
   }
 
   _addLock() {
@@ -102,8 +141,8 @@ export class Modal {
     this.bodyElement.style.overflow = "hidden";
 
     if (scrollWidth) {
-      this.bodyElement.style.paddingRight = scrollWidth + "px"
-    };
+      this.bodyElement.style.paddingRight = scrollWidth + "px";
+    }
   }
 
   _removeLock() {
