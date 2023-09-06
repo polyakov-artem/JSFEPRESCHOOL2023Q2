6c7b403a05b9b7db20b338d5682c958b1295047d
polyakov-artem <Polyakov.Artem.V@yandex.ru>
2023-09-06 20:31:02 +0300

    feat: add new function calls

--- library/js/index.js
+++ library/js/index.js
@@ -1,14 +1,38 @@
-import "./headerNavMenu.js";
-import {Modal} from "./modal.js"
-import {Dropdown} from "./dropdown.js"
+import { HeaderNavMenu } from "./headerNavMenu.js";
+import { Dropdown } from "./dropdown.js";
+import { Modal } from "./modal.js";
+import { App } from "./app.js";
+import { Server } from "./server.js";
+import { UserMenu } from "./userMenu.js";
+import { Signup } from "./signup.js";
+import { Login } from "./login.js";
+
+const SELECTOR_SIGNUP_FORM = ".signup-form";
+const SELECTOR_LOGIN_FORM = ".login-form";
 
 document.addEventListener("DOMContentLoaded", () => {
-    new Dropdown({
-        classDropdown: 'user-menu',
-        classToggler: 'user-menu__avatar-wrap',
-        classDropdownActive: 'user-menu_active',
-        closeClasses: ['user-menu__link']
-    });
+  const server = new Server();
+  new HeaderNavMenu();
 
-    new Modal();
+  new Dropdown({
+    classDropdown: "user-menu",
+    classToggler: "user-menu__avatar-wrap",
+    classDropdownActive: "user-menu_active",
+    closeClasses: ["user-menu__link"],
+  });
+
+  new Modal();
+  new UserMenu();
+
+  
+  const app = new App(server);
+
+  document.querySelectorAll(SELECTOR_SIGNUP_FORM).forEach((form) => {
+    new Signup(form, app);
+  });
+
+  document.querySelectorAll(SELECTOR_LOGIN_FORM).forEach((form) => {
+    new Login(form, app);
+  });
+  
 });
