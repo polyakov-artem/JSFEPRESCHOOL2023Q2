dac812619181b1b56669b7931ea72fc2030af246
polyakov-artem <Polyakov.Artem.V@yandex.ru>
2023-09-06 19:51:28 +0300

    feat: add signup block template

---
+++ library/js/signup.js
@@ -0,0 +1,69 @@
+export class Signup {
+  constructor(form, app){
+    this._form = form;
+    this._app = app;
+    // this.data = {};
+    // this.isValid = false;
+
+    this._bindEvents();
+  }
+
+  _bindEvents(){
+    this._form.addEventListener("submit", this._send.bind(this));
+  }
+
+  _send(e){
+    e.preventDefault();
+    
+    this._app.signup({
+      firstName: "John",
+      lastName: "Doe",
+      email: "some@mail.ri",
+      password: "123",
+    });
+
+    return false;
+  }
+
+  // _submitHandler(e){
+  //   e.preventDefault();
+  //   console.log(this.form.elements);
+
+  //   this._getFormData();
+  //   this._validateForm();
+
+  //   if (this.isValid) {
+  //     this._getData();
+  //     this._sendData();
+  //     this._clear();
+  //   };
+  // }
+
+  // _getFormData(){
+  //   const formData = {
+
+  //   }
+  //   this.formData = formData;
+  // }
+
+  // _getData(){
+  //   for (let key in this.formData){
+  //     this.data[key] = this.formData[key].value
+  //   }
+  // }
+
+  // _validateForm(){
+  //   this.isValid = true;
+  // }
+
+
+  // _sendData(){
+  //   document.dispatchEvent(new CustomEvent("eSignup", {
+  //     detail: this.data
+  //   }))
+  // }
+
+  // _clear(){
+
+  // }
+}
