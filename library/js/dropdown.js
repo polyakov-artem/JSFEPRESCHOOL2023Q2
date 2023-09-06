94d8501e3c9f3983941a618289724fff58fa5f49
polyakov-artem <Polyakov.Artem.V@yandex.ru>
2023-09-06 19:45:31 +0300

    refactor: fix code style in dropdown.js

--- library/js/dropdown.js
+++ library/js/dropdown.js
@@ -1,74 +1,69 @@
 export class Dropdown {
-    constructor(props = {}) {
-        this.classDropdown = props.classDropdown || 'dropdown';
-        this.classToggler = props.classToggler || 'dropdown__toggler';
-        this.classDropdownActive = props.classDropdownActive || 'dropdown_active';
-        this.closeClasses = props.closeClasses || [];
-        this.closeOnOutsideClick = props.closeOnOutsideClick || true;
-        this.activeDropdown = null;
+  constructor(props = {}) {
+    this.classDropdown = props.classDropdown || "dropdown";
+    this.classToggler = props.classToggler || "dropdown__toggler";
+    this.classDropdownActive = props.classDropdownActive || "dropdown_active";
+    this.closeClasses = props.closeClasses || [];
+    this.closeOnOutsideClick = props.closeOnOutsideClick || true;
+    this.activeDropdown = null;
 
-        this.bindEvents()
+    this.bindEvents();
+  }
+
+  bindEvents() {
+    document.addEventListener("click", this._clickHandler.bind(this));
+  }
+
+  _clickHandler(e) {
+    const element = e.target;
+    const toggler = element.closest(`.${this.classToggler}`);
+    const dropdown = element.closest(`.${this.classDropdown}`);
+    const isNotDropdown = !dropdown;
+
+    if (toggler && dropdown) {
+      e.preventDefault();
+
+      this.activeDropdown = dropdown;
+
+      dropdown.classList.contains(this.classDropdownActive)
+        ? this._close()
+        : this._open();
+
+      return;
     }
 
-    bindEvents() {
-        document.addEventListener("click", this._clickHandler.bind(this));
+    if (
+      (this.activeDropdown && this.closeOnOutsideClick && isNotDropdown) ||
+      this._hasParentWithClass(element, this.closeClasses)
+    ) {
+      this._close();
+      return;
+    }
+  }
+
+  _hasParentWithClass(element, classes) {
+    if (!classes.length) return false;
+    let parent = element;
+
+    while (parent !== document) {
+      if (classes.some((className) => parent.classList.contains(className))) {
+        return true;
+      };
+      parent = parent.parentNode;
     }
 
-    _clickHandler(e) {
-        const element = e.target;
-        const toggler = element.closest(`.${this.classToggler}`);
-        const dropdown = element.closest(`.${this.classDropdown}`);
-        const isNotDropdown = !dropdown;
+    return false;
+  }
 
-        if (toggler && dropdown) {
-            e.preventDefault();
+  _open() {
+    if (this.activeDropdown)
+      this.activeDropdown.classList.add(this.classDropdownActive);
+  }
 
-            this.activeDropdown = dropdown;
-
-            dropdown.classList.contains(this.classDropdownActive) ?
-                this._close() :
-                this._open();
-
-            return;
-        };
-
-        if (
-            this.activeDropdown && 
-            this.closeOnOutsideClick && isNotDropdown || 
-            this._hasParentWithClass(element, this.closeClasses)  
-            ) {
-            this._close();
-            return
-        };
+  _close() {
+    if (this.activeDropdown) {
+      this.activeDropdown.classList.remove(this.classDropdownActive);
+      this.activeDropdown = null;
     }
-    
-    _hasParentWithClass (element, classes) {
-        if (!classes.length) return false;
-
-        let parent = element;
-
-        
-        while (parent !== document) {
-          console.log(parent);
-          if (classes.some(className => parent.classList.contains(className))) {
-            return true;
-          };
-          
-          parent = parent.parentNode;
-        };
-
-        return false; 
-    }
-
-    _open() {
-        if (this.activeDropdown) this.activeDropdown.classList.add(this.classDropdownActive);
-    }
-
-    _close() {
-        if (this.activeDropdown){
-            this.activeDropdown.classList.remove(this.classDropdownActive);
-            this.activeDropdown = null;
-        }
-    }
-};
-
+  }
+}
