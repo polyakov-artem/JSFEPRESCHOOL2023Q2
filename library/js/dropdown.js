206dd2cd38910572af30578e6d6f8d3d1ac627fa
polyakov-artem <Polyakov.Artem.V@yandex.ru>
2023-09-10 22:59:18 +0300

    refactor: refactored dropdown code

--- library/js/dropdown.js
+++ library/js/dropdown.js
@@ -3,8 +3,6 @@ export class Dropdown {
     this.classDropdown = props.classDropdown || "dropdown";
     this.classToggler = props.classToggler || "dropdown__toggler";
     this.classDropdownActive = props.classDropdownActive || "dropdown_active";
-    this.closeClasses = props.closeClasses || [];
-    this.closeOnOutsideClick = props.closeOnOutsideClick || true;
     this.activeDropdown = null;
 
     this.bindEvents();
@@ -16,6 +14,7 @@ export class Dropdown {
 
   _clickHandler(e) {
     const element = e.target;
+    const elementTag = element.tagName;
     const toggler = element.closest(`.${this.classToggler}`);
     const dropdown = element.closest(`.${this.classDropdown}`);
     const isNotDropdown = !dropdown;
@@ -32,38 +31,18 @@ export class Dropdown {
       return;
     }
 
-    if (
-      (this.activeDropdown && this.closeOnOutsideClick && isNotDropdown) ||
-      this._hasParentWithClass(element, this.closeClasses)
-    ) {
+    if ((this.activeDropdown && elementTag === "A") || isNotDropdown)
       this._close();
-      return;
-    }
-  }
-
-  _hasParentWithClass(element, classes) {
-    if (!classes.length) return false;
-    let parent = element;
-
-    while (parent !== document) {
-      if (classes.some((className) => parent.classList.contains(className))) {
-        return true;
-      };
-      parent = parent.parentNode;
-    }
-
-    return false;
   }
 
   _open() {
-    if (this.activeDropdown)
-      this.activeDropdown.classList.add(this.classDropdownActive);
+    this.activeDropdown.classList.add(this.classDropdownActive);
   }
 
   _close() {
-    if (this.activeDropdown) {
-      this.activeDropdown.classList.remove(this.classDropdownActive);
-      this.activeDropdown = null;
-    }
+    if (!this.activeDropdown) return;
+
+    this.activeDropdown.classList.remove(this.classDropdownActive);
+    this.activeDropdown = null;
   }
 }
