a783dd25d85b8d3f7b0d50c2885385ec56603272
polyakov-artem <Polyakov.Artem.V@yandex.ru>
2023-09-10 22:55:51 +0300

    fix: fixed user menu

--- library/js/userMenu.js
+++ library/js/userMenu.js
@@ -7,58 +7,80 @@ const CLASS_GUEST_AVATAR = "user-menu__guest-avatar";
 const CLASS_USER_AVATAR = "user-menu__user-avatar";
 const ATTRIBUTE_MODAL = "data-modal-id";
 const VALUE_MODAL = "modal-profile";
-const ATTRIBUTE_AUTH = "data-action";
+const ATTRIBUTE_LOGOUT = "data-action";
 const VALUE_LOGOUT = "logout";
 
 
-export class UserMenu{
-  constructor(){
-    this._loggedIn = false;
-    this._domUserMenu = null;
-    this._domGuestMenu = document.querySelector(`.${CLASS_MENU}`).cloneNode(true);
+export class UserMenu {
+  constructor() {
+    this._guestMenu = document.querySelector(`.${CLASS_MENU}`).cloneNode(true);
+    this._userMenu = this._createMenuTemplate();
+
     this._bindEvents();
   }
-
-  _bindEvents(){
-    document.addEventListener("login", this._loginHandler.bind(this))
-    document.addEventListener("logout", this._logoutHandler.bind(this))
+  
+  _bindEvents() {
+    document.addEventListener("serverResponse",this._serverResponseHandler.bind(this));
+    document.addEventListener("click", this._logoutBtnHandler.bind(this));
   }
 
-  _logoutHandler(){
-    this._loggedIn = false;
-    this._domUserMenu = null;
-    document.querySelector(`.${CLASS_MENU}`).replaceWith(this._domGuestMenu);
+  _serverResponseHandler(e) {
+    const response = e.detail;
+    if (!response.done) return;
+
+    switch (response.type) {
+      case "login":
+        this._changeToUserMenu(response.data);
+        break;
+      case "logout":
+        this._changeToGuestMenu();
+        break;
+    }
   }
 
-  _loginHandler(e){
-    const userInfo = e.detail;
-    this._loggedIn = true;
-    this._domUserMenu = this._createUserMenu(userInfo);
-    document.querySelector(`.${CLASS_MENU}`).replaceWith(this._domUserMenu);
+  _logoutBtnHandler(e){
+    const logoutBtn = e.target.closest(`[${ATTRIBUTE_LOGOUT}="${VALUE_LOGOUT}"]`);
+    if (!logoutBtn) return;
+    e.preventDefault();
+    document.dispatchEvent(new CustomEvent("clientSubmit", {detail:{type: "logout"}}))
   }
 
-  _createUserMenu(userInfo){
-    const domUserMenu = this._domGuestMenu.cloneNode(true);
+  _changeToGuestMenu() {
+    document.querySelector(`.${CLASS_MENU}`).replaceWith(this._guestMenu);
+  }
 
-    const title = domUserMenu.querySelector(`.${CLASS_MENU_TITLE}`);
-    title.textContent = userInfo.card;
+  _changeToUserMenu(userInfo) {
+    this._updateUserMenu(userInfo);
+    document.querySelector(`.${CLASS_MENU}`).replaceWith(this._userMenu);
+  }
+
+  _createMenuTemplate(){
+    const menu = this._guestMenu.cloneNode(true);
+    const title = menu.querySelector(`.${CLASS_MENU_TITLE}`);
     title.classList.add(CLASS_MENU_TITLE_SMALL);
+    
+    const userAvatar = document.createElement("div");
+    userAvatar.classList.add(CLASS_USER_AVATAR);
+    menu.querySelector(`.${CLASS_GUEST_AVATAR}`).replaceWith(userAvatar);
 
-    const domUserAvatar = document.createElement('div');
-    domUserAvatar.textContent = `${userInfo.firstName.toUpperCase()[0]}${userInfo.lastName.toUpperCase()[0]}`;
-    domUserAvatar.classList.add(CLASS_USER_AVATAR);
-    domUserMenu.querySelector(`.${CLASS_GUEST_AVATAR}`).replaceWith(domUserAvatar);
-
-    const links = domUserMenu.querySelectorAll(`.${CLASS_MENU_CONTENT} .${CLASS_MENU_LINK}`);
+    const links = menu.querySelectorAll(`.${CLASS_MENU_CONTENT} .${CLASS_MENU_LINK}`);
     links[0].textContent = "My profile";
     links[0].setAttribute(`${ATTRIBUTE_MODAL}`, `${VALUE_MODAL}`);
 
     links[1].textContent = "Log Out";
-    links[1].setAttribute(`${ATTRIBUTE_AUTH}`, `${VALUE_LOGOUT}`);
+    links[1].setAttribute(`${ATTRIBUTE_LOGOUT}`, `${VALUE_LOGOUT}`);
     links[1].removeAttribute(`${ATTRIBUTE_MODAL}`);
 
-    return domUserMenu;
+    return menu;
+  };
+
+  _updateUserMenu(userInfo) {
+    const title =this._userMenu.querySelector(`.${CLASS_MENU_TITLE}`);
+    title.textContent = userInfo.card;
+    
+    const userAvatar =this._userMenu.querySelector(`.${CLASS_USER_AVATAR}`);
+    userAvatar.setAttribute("title", `${userInfo.firstName} ${userInfo.lastName}`)
+    userAvatar.textContent = `${userInfo.firstName.toUpperCase()[0]}${userInfo.lastName.toUpperCase()[0]}`;
   }
-  
- }
+}
 
