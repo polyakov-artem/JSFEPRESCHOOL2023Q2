079a775ec9efa4a581fd298058705d45f94371f3
polyakov-artem <Polyakov.Artem.V@yandex.ru>
2023-09-13 20:17:08 +0300

    fixL email validation

--- library/js/signup.js
+++ library/js/signup.js
@@ -79,10 +79,9 @@ export class Signup {
             errorText = "Enter at least 8 characters";
           break;
         case "email":
-          const regexp =
-            /(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-          if (!regexp.test(input.value)) 
-            errorText = "Enter valid email";
+          let regexp =
+            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
+          if (!regexp.test(input.value)) errorText = "Enter valid email";
           break;
       }
 
