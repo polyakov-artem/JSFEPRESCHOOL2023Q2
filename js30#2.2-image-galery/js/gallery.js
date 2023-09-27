034950df031ab4bb20a7b44c4806b259ff601cb1
polyakov-artem <Polyakov.Artem.V@yandex.ru>
2023-09-27 23:46:00 +0300

    feat: add gallery functionality

--- js30#2.2-image-galery/js/gallery.js
+++ js30#2.2-image-galery/js/gallery.js
@@ -1,11 +1,58 @@
+const REQUEST_URL = "https://api.unsplash.com/search/photos";
+const CLIENT_ID = "hPl2TqgWM69XM6_HKdYCvh-D-DOH7PId5GU26MYNZpQ";
+
 export class Gallery {
   constructor(element) {
+    this._domGallery = element;
+    this._domSearchForm = document.querySelector(".header__search");
+    this._domSearchInput = this._domSearchForm.querySelector(".input__control");
+    this._domImgContainer = element.querySelector(".gallery__grid");
 
     this._bindEvents();
   }
 
   _bindEvents() {
+    this._domSearchForm.addEventListener("submit",this._submitHandler.bind(this));
+  }
 
+  _submitHandler(e) {
+    e.preventDefault();
+
+    let query = this._domSearchInput.value.trim();
+    if (query.length == 0) return;
+
+    const url = `${REQUEST_URL}?query=${query}&client_id=${CLIENT_ID}&per_page=30`;
+    const imgContainerNotEmpty = this._domImgContainer.children.length != 0;
+    if (imgContainerNotEmpty) this._domImgContainer.replaceChildren();
+    
+    this._createImages(url);
+  }
+
+  async _getData(url) {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      return await response.json();
+    } else {
+      alert("Ошибка HTTP: " + response.status);
+    }
+  }
+
+  async _createImages(url) {
+    const json = await this._getData(url);
+    if (!json) return;
+
+    const tempContainer = new DocumentFragment();
+
+    json.results.forEach((imageInfo) => {
+      const img = document.createElement("img");
+      img.classList.add("gallery__item");
+      img.alt = imageInfo.alt_description;
+      img.src = imageInfo.urls.regular;
+      tempContainer.append(img);
+    });
+
+    this._domImgContainer.append(tempContainer);
   }
 }
 
